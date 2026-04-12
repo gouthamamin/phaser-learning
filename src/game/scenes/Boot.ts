c574c6b4bb3cf5058fe4c4060739c765cfbeb288
Goutham Amin <gouthamamin@gmail.com>
2026-04-12 17:54:24 +0530

    feat: load & add image

--- src/game/scenes/Boot.ts
+++ src/game/scenes/Boot.ts
@@ -6,33 +6,16 @@ export class Boot extends Scene{
     }
 
     init(){
-        console.log("Boot Init");
     }
 
     preload () {
         //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
         //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
         this.load.image('background', 'assets/bg.png');
-        console.log("Boot preload");
     }
 
     create (){
         console.log("Boot create");
-
-        this.events.on("shutdown", this.shutdown, this);
-        this.events.on("destroy", this.destroy, this);
         this.scene.start('Preloader');
-    }
-
-    destroy(){
-        console.log("destroy() : Boot scene is removed from memory")
-    }
-
-    shutdown(){
-        console.log("shutdown() : Boot scene is not current scene anymore")
-    }
-
-    update(){
-        console.log("Boot: update running")
     }
 }
